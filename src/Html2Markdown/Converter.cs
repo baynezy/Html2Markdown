ca100372e496712fc5e4cccd00c3fab67755c5d3
Simon Baynes <baynezy@gmail.com>
2013-07-03 07:39:27 +0100

    Created initial setup
    Added support for strong tags
    
    Added support for b tags
    
    Added support for emphasis and italic
    
    Added conversion for <br/> tags
    
    Added support for <code> tag
    
    Added support for headers
    
    Added nuspec and setup version number

---
+++ src/Html2Markdown/Converter.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Html2Markdown
+{
+	public class Converter
+	{
+		private readonly IList<Element> _elements = new List<Element>
+			{
+				new Element{
+					Pattern = @"<a.+?href\s*=\s*['""]([^'""]+)['""]>([^<]+)</a>",
+					Replacement = @"[$2]($1)"
+				},
+				new Element{
+					Pattern = @"</?(strong|b)>",
+					Replacement = @"**"
+				},
+				new Element{
+					Pattern = @"</?(em|i)>",
+					Replacement = @"*"
+				},
+				new Element{
+					Pattern = @"<br\s/>",
+					Replacement = @"  " + System.Environment.NewLine
+				},
+				new Element{
+					Pattern = @"</?code>",
+					Replacement = @"`"
+				},
+				new Element{
+					Pattern = @"</h[1-6]>",
+					Replacement = System.Environment.NewLine + System.Environment.NewLine
+				},
+				new Element{
+					Pattern = @"<h1>",
+					Replacement = System.Environment.NewLine + System.Environment.NewLine + "# "
+				},
+				new Element{
+					Pattern = @"<h2>",
+					Replacement = System.Environment.NewLine + System.Environment.NewLine + "## "
+				},
+				new Element{
+					Pattern = @"<h3>",
+					Replacement = System.Environment.NewLine + System.Environment.NewLine + "### "
+				},
+				new Element{
+					Pattern = @"<h4>",
+					Replacement = System.Environment.NewLine + System.Environment.NewLine + "#### "
+				},
+				new Element{
+					Pattern = @"<h5>",
+					Replacement = System.Environment.NewLine + System.Environment.NewLine + "##### "
+				},
+				new Element{
+					Pattern = @"<h6>",
+					Replacement = System.Environment.NewLine + System.Environment.NewLine + "###### "
+				}
+			};
+
+		public string Convert(string html)
+		{
+			return _elements.Aggregate(html, (current, element) => ReplacePattern(current, element.Pattern, element.Replacement));
+		}
+
+		private static string ReplacePattern(string html, string pattern, string replacement)
+		{
+			var regex = new Regex(pattern);
+
+			return regex.Replace(html, replacement);
+		}
+	}
+
+	internal class Element
+	{
+		public string Pattern { get; set; }
+
+		public string Replacement { get; set; }
+	}
+}
---
+++ test/Html2Markdown.Test/ConverterTest.cs
@@ -0,0 +1,177 @@
+﻿using NUnit.Framework;
+
+namespace Html2Markdown.Test
+{
+	[TestFixture]
+	class ConverterTest
+	{
+		[Test]
+		public void Convert_WhenThereAreHtmlLinks_ThenConvertToMarkDownLinks()
+		{
+			const string html = @"So this is <a href=""http://www.simonbaynes.com/"">a link</a>. Convert it";
+			const string expected = @"So this is [a link](http://www.simonbaynes.com/). Convert it";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreMultipleHtmlLinks_ThenConvertThemToMarkDownLinks()
+		{
+			const string html = @"So this is <a href=""http://www.simonbaynes.com/"">a link</a> and so is <a href=""http://www.google.com/"">this</a>. Convert them";
+			const string expected = @"So this is [a link](http://www.simonbaynes.com/) and so is [this](http://www.google.com/). Convert them";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreStrongTags_ThenConvertToMarkDownDoubleAsterisks()
+		{
+			const string html = @"So this text is <strong>bold</strong>. Convert it.";
+			const string expected = @"So this text is **bold**. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreMultipleStrongTags_ThenConvertToMarkDownDoubleAsterisks()
+		{
+			const string html = @"So this text is <strong>bold</strong> and <strong>is this</strong>. Convert it.";
+			const string expected = @"So this text is **bold** and **is this**. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreBoldTags_ThenConvertToMarkDownDoubleAsterisks()
+		{
+			const string html = @"So this text is <b>bold</b>. Convert it.";
+			const string expected = @"So this text is **bold**. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreEmphsisTags_ThenConvertToMarkDownSingleAsterisk()
+		{
+			const string html = @"So this text is <em>italic</em>. Convert it.";
+			const string expected = @"So this text is *italic*. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreItalicTags_ThenConvertToMarkDownSingleAsterisk()
+		{
+			const string html = @"So this text is <i>italic</i>. Convert it.";
+			const string expected = @"So this text is *italic*. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreBreakTags_ThenConvertToMarkDownDoubleSpacesWitCarriageReturns()
+		{
+			const string html = @"So this text has a break.<br />Convert it.";
+			const string expected = @"So this text has a break.  
+Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreCodeTags_ThenReplaceWithBackTick()
+		{
+			const string html = @"So this text has code <code>alert();</code>. Convert it.";
+			const string expected = @"So this text has code `alert();`. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreH1Tags_ThenReplaceWithMarkDownHeader()
+		{
+			const string html = @"This code has a <h1>header</h1>. Convert it.";
+			const string expected = @"This code has a 
+
+# header
+
+. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreH2Tags_ThenReplaceWithMarkDownHeader()
+		{
+			const string html = @"This code has a <h2>header</h2>. Convert it.";
+			const string expected = @"This code has a 
+
+## header
+
+. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreH3Tags_ThenReplaceWithMarkDownHeader()
+		{
+			const string html = @"This code has a <h3>header</h3>. Convert it.";
+			const string expected = @"This code has a 
+
+### header
+
+. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreH4Tags_ThenReplaceWithMarkDownHeader()
+		{
+			const string html = @"This code has a <h4>header</h4>. Convert it.";
+			const string expected = @"This code has a 
+
+#### header
+
+. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreH5Tags_ThenReplaceWithMarkDownHeader()
+		{
+			const string html = @"This code has a <h5>header</h5>. Convert it.";
+			const string expected = @"This code has a 
+
+##### header
+
+. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		[Test]
+		public void Convert_WhenThereAreH6Tags_ThenReplaceWithMarkDownHeader()
+		{
+			const string html = @"This code has a <h6>header</h6>. Convert it.";
+			const string expected = @"This code has a 
+
+###### header
+
+. Convert it.";
+
+			CheckConversion(html, expected);
+		}
+
+		private static void CheckConversion(string html, string expected)
+		{
+			var converter = new Converter();
+
+			var result = converter.Convert(html);
+
+			Assert.That(result, Is.EqualTo(expected));
+		}
+	}
+}
