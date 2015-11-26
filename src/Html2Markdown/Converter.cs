d93139716a996e4c0d54a5498019dab9cccf83f0
Simon Baynes <baynezy@gmail.com>
2015-11-26 03:18:03 +0000

    Removed extra whitespace. Closes #13

--- src/Html2Markdown/Converter.cs
+++ src/Html2Markdown/Converter.cs
@@ -144,7 +144,10 @@ namespace Html2Markdown
 		private static string CleanWhiteSpace(string markdown)
 		{
 			var cleaned = Regex.Replace(markdown, @"\r\n\s+\r\n", "\r\n\r\n");
-			return Regex.Replace(cleaned, @"(?:\r\n){3,}", "\r\n\r\n");
+			cleaned = Regex.Replace(cleaned, @"(?:\r\n){3,}", "\r\n\r\n");
+			cleaned = Regex.Replace(cleaned, @"^(\r\n)+", "");
+			cleaned = Regex.Replace(cleaned, @"(\r\n)+$", "");
+			return cleaned;
 		}
 	}
 }
