4b1e3c013e77f711b258a49e3441df5264b061c4
baynezy <baynezy@gmail.com>
2024-09-06 15:50:09 +0000

    Deploying to gh-pages from @ baynezy/Html2Markdown@a1b6512b7d53ee3c51dfce8aea8fd0ca11c20ea0 🚀

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['addreplacementgroup_0',['AddReplacementGroup',['../class_html2_markdown_1_1_scheme_1_1_abstract_scheme.html#a709edbf8966fc43b6e748a69f61f1f61',1,'Html2Markdown::Scheme::AbstractScheme']]],
-  ['addreplacer_1',['AddReplacer',['../class_html2_markdown_1_1_replacement_1_1_composite_replacer.html#a35eefde7b32441629ef82a9c94cecd06',1,'Html2Markdown::Replacement::CompositeReplacer']]],
+  ['addreplacer_1',['AddReplacer',['../class_html2_markdown_1_1_replacement_1_1_composite_replacer.html#a35eefde7b32441629ef82a9c94cecd06',1,'Html2Markdown.Replacement.CompositeReplacer.AddReplacer()'],['../class_html2_markdown_1_1_scheme_1_1_abstract_scheme.html#ab32df6bd81c999554ba06992f64316ff',1,'Html2Markdown.Scheme.AbstractScheme.AddReplacer()']]],
   ['anchortagreplacer_2',['AnchorTagReplacer',['../class_html2_markdown_1_1_replacement_1_1_anchor_tag_replacer.html#addf3273d7f9b6709900c2d309ac8e3e1',1,'Html2Markdown::Replacement::AnchorTagReplacer']]]
 ];
